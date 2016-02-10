fa241bc2eccbe2ee2083decd4fd31529feab3c7b
a.prokopyev <a.prokopyev@qiwi.kz>
2016-02-10 18:35:47 +0600

    Использование приложения dashboard вместо статичной конфигурации, удалены старые компоненты

--- scripts/pages/dashboard.js
+++ scripts/pages/dashboard.js
@@ -1,69 +1,17 @@
-import ko from 'knockout';
 import DataClient from 'lib/dataClient';
-import toastr from 'toastr';
+import DashboardApp from 'ext/dashboard/app';
 import utils from 'lib/utils';
 
-import 'widgets/text';
-import 'widgets/date';
-import 'widgets/chart';
-import 'widgets/qrcode';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Grid from 'components/grid';
+import TextWidget from 'components/textWidget';
+import DateWidget from 'components/dateWidget';
+import QRCodeWidget from 'components/qrcodeWidget';
+import ConnectClientWidget from 'components/connectClientWidget';
 
-import BaseDataSource from 'data-sources/base';
-import DateDataSource from 'data-sources/date';
-import ServerDataSource from 'data-sources/serverData';
-import SimpleGetDataSource from 'data-sources/simpleGet';
+import DateTimeSource from 'data-sources/date';
 import ClientIdDataSource from 'data-sources/clientId';
-
-import UafApp from 'lib/uafApp';
-
-window.BaseDataSource = BaseDataSource;
-window.DateDataSource = DateDataSource;
-window.ServerDataSource = ServerDataSource;
-window.SimpleGetDataSource = SimpleGetDataSource;
-window.ClientIdDataSource = ClientIdDataSource;
-
-class DashboardApp extends UafApp {
-  constructor(dataClient, options) {
-    super(dataClient, options);
-  }
-
-  begin() {
-    this.registerApp('Dashboard', 'dashboard.client.html');
-  }
-
-  onApplicationRegistred() {
-    console.log('Application registred');
-  }
-
-  onClientConnected(clientId) {
-    console.log('Client ' + clientId + ' connected');
-    toastr.info('Client ' + clientId + ' connected', 'System');
-  }
-
-  onClientDisconnected(clientId) {
-    console.log('Client ' + clientId + ' disconnected');
-    toastr.info('Client ' + clientId + ' disconnected', 'System');
-  }
-
-  onClientMessage(clientId, data) {
-    console.log('Message from ' + clientId, data);
-    if (data.command == 'showToast') {
-      toastr.info(data.message, 'Message');
-    } else if (data.command == 'showStatic') {
-      this.dataClient.local('page.staticMessage', { data: data.message });
-    } else if (data.command == 'reloadPage') {
-      this.requestReloadCode(reloadCode => {
-        var newLocation = location.origin + location.pathname + '?reloadCode=' + reloadCode;
-        location.href = newLocation;
-      });
-    } else if (data.command == 'goPage') {
-      this.requestReloadCode(reloadCode => {
-        var newLocation = location.origin + '/' + data.url + '?reloadCode=' + reloadCode;
-        location.href = newLocation;
-      });
-    }
-  }
-}
 
 var client = new DataClient();
 DataClient.default = client;
@@ -75,5 +23,3 @@ var qs = utils.parseQueryString();
 if (qs.reloadCode) {
   app.restoreClients(qs.reloadCode);
 }
-
-ko.applyBindings();
